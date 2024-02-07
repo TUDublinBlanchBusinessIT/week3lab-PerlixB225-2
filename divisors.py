000c6f1520abe61f0d4d4f108c088d79c020ab2b
B00105225 Perlix Soco <B00105225@mytudublin.ie>
2024-02-07 11:35:02 +0000

    step 1 done

--- divisors.py
+++ divisors.py
@@ -1,6 +1,6 @@
-#Add your name here
-#Add the date here
-#Add the filename here
+#Perlix Soco
+#07.02.2024
+#divisors.py
 
 #Add a function below called divisors(num) which takes one argument of type integer
 #and returns a list of all the divisors(factors) of that that number -
@@ -19,3 +19,14 @@
  
     #return the list
     
+def divisors(num):
+
+    myList = []
+
+    for i in range(1, num):
+        if num % i == 0:
+
+            myList.append(i)
+    return myList
+
+print(divisors(28))
